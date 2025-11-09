681b9416b88ddd0b7c6b28c0e27beddd537817e4
ngocbq3 <43702911+ngocbq3@users.noreply.github.com>
2025-11-09 21:47:31 +0700

    Update AuthController.php

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
             'email'    => 'required|email|unique:users,email',
             'password' => 'required|min:6|confirmed', // password_confirmation required
         ]);
-
+        //Thông báo lỗi khi nhập sai dữ liệu
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()], 422);
         }
